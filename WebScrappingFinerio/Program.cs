639d37d0750be0faa56181cdee90430e88f83ed8
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-09 19:31:06 -0500

    adding functions for insert in BD

--- WebScrappingFinerio/Program.cs
+++ WebScrappingFinerio/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using WebScrapingFinerio;
 
 namespace WebScrappingFinerio
@@ -9,52 +10,78 @@ namespace WebScrappingFinerio
         public static void Main(string[] args)
         {
             string filterValue = "";
+            List<string> ListInfo = new List<string>();
 
             GetInfoWeb GetInfoWeb = new GetInfoWeb();
             ShowInfoConsole ShowInfoConsole = new ShowInfoConsole();
             DataBaseConnection DataBaseConnection = new DataBaseConnection();
 
+
             ShowInfoConsole.ShowMenu();
 
-            //GenreAndSubGenre
-            /*filterValue = "rock";
-            GetInfoWeb.NameClass = ".genre-item";
-            GetInfoWeb.NameURI = $"https://us.napster.com/genre/{filterValue}";
-            */
+            switch (Console.ReadLine())
+            {
+
+                case "1":
+                    //GenreAndSubGenre
+                    Console.WriteLine("GenreAndSubGenre, type Genre:");
+                    filterValue = Console.ReadLine();
+                    GetInfoWeb.NameClass = ".genre-item";
+                    GetInfoWeb.NameURI = $"https://us.napster.com/genre/{filterValue}";
+
+                    ListInfo = GetInfoWeb.GetInfoFilter();
+                    //ShowInfoConsole.showInfoFunc(ListInfo);
+                    DataBaseConnection.GenreAndSubGenre(filterValue, ListInfo);
+                   
+
+                    break;
+                case "2":
+                    //ArtistByGenre
+                    Console.WriteLine("ArtistByGenre, type Genre:");
+                    filterValue = Console.ReadLine();
+                    GetInfoWeb.NameClass = ".artist_name";
+                    GetInfoWeb.NameURI = $"https://us.napster.com/genre/{filterValue}";
+
+                    ListInfo = GetInfoWeb.GetInfoFilter();
+                    //ShowInfoConsole.showInfoFunc(ListInfo);
+                    DataBaseConnection.ArtistByGenre(filterValue, ListInfo);
+
+                    break;
+                case "3":
+                    //AlbumByArtist
+                    Console.WriteLine("AlbumByArtist, type Artist:");
+                    filterValue = Console.ReadLine();
+                    GetInfoWeb.NameClass = ".album_name";
+                    GetInfoWeb.NameURI = $"https://us.napster.com/artist/{filterValue}";
+
+                    break;
+                case "4":
+                    //SongsByAlbum
+                    Console.WriteLine("SongsByAlbum, type song:");
+                    filterValue = Console.ReadLine();
+                    GetInfoWeb.NameClass = ".artist-name";
+                    GetInfoWeb.NameURI = $"https://us.napster.com/search/track?query={filterValue}";
+
+                    break;
+                case "5":
+                    
+                    //ArtistBySong
+                    Console.WriteLine("ArtistBySong: type artist");
+                    filterValue = Console.ReadLine();
+                    Console.WriteLine("SongsByAlbum: type song");
+                    string filterValue2 = Console.ReadLine();
+                    GetInfoWeb.NameClass = ".no-artist";
+                    GetInfoWeb.NameURI = $"https://us.napster.com/artist/{filterValue}/album/{filterValue2}";
 
 
-            //ArtistByGenre
-            /*
-             * filterValue = "rock";
+                    break;
 
-            GetInfoWeb.NameClass = ".artist_name";
-            GetInfoWeb.NameURI = $"https://us.napster.com/genre/{filterValue}";
+                default:
+                    break;
 
-            var ListInfo= GetInfoWeb.GetInfoFilter();
-            DataBaseConnection.DataBaseConnectionList(ListInfo);
-            */
+            }
 
-            //AlbumByArtist
-            /*filterValue = "60-tigres";
-            GetInfoWeb.NameClass = ".album_name";
-            GetInfoWeb.NameURI = $"https://us.napster.com/artist/{filterValue}";
-            //List<string> filteredList = GetInfoWeb.GetInfoFilter();*/
-
-
-            //SongsByAlbum
-            /*filterValue = "60-tigres";
-            string filterValue2 = "frenesi";
-            GetInfoWeb.NameClass = ".no-artist";
-            GetInfoWeb.NameURI = $"https://us.napster.com/artist/{filterValue}/album/{filterValue2}";
-            //List<string> filteredList = GetInfoWeb.GetInfoFilter();*/
-
-            //ArtistBySong
-            /*filterValue = "viento";
-            GetInfoWeb.NameClass = ".artist-name";
-            GetInfoWeb.NameURI = $"https://us.napster.com/search/track?query={filterValue}";
-            //List<string> filteredList = GetInfoWeb.GetInfoFilter();*/
-
-
+            Console.WriteLine("*********Info Added on DataBase*********");
 
 
 
@@ -64,6 +91,49 @@ namespace WebScrappingFinerio
     }
 
 
-
-
 }
+
+
+
+
+//GenreAndSubGenre
+/*filterValue = "rock";
+GetInfoWeb.NameClass = ".genre-item";
+GetInfoWeb.NameURI = $"https://us.napster.com/genre/{filterValue}";
+*/
+
+
+//ArtistByGenre
+/*
+ * filterValue = "rock";
+
+GetInfoWeb.NameClass = ".artist_name";
+GetInfoWeb.NameURI = $"https://us.napster.com/genre/{filterValue}";
+
+var ListInfo= GetInfoWeb.GetInfoFilter();
+DataBaseConnection.DataBaseConnectionList(ListInfo);
+*/
+
+//AlbumByArtist
+/*filterValue = "60-tigres";
+GetInfoWeb.NameClass = ".album_name";
+GetInfoWeb.NameURI = $"https://us.napster.com/artist/{filterValue}";
+//List<string> filteredList = GetInfoWeb.GetInfoFilter();*/
+
+
+//SongsByAlbum
+/*filterValue = "60-tigres";
+string filterValue2 = "frenesi";
+GetInfoWeb.NameClass = ".no-artist";
+GetInfoWeb.NameURI = $"https://us.napster.com/artist/{filterValue}/album/{filterValue2}";
+//List<string> filteredList = GetInfoWeb.GetInfoFilter();*/
+
+//ArtistBySong
+/*filterValue = "viento";
+GetInfoWeb.NameClass = ".artist-name";
+GetInfoWeb.NameURI = $"https://us.napster.com/search/track?query={filterValue}";
+//List<string> filteredList = GetInfoWeb.GetInfoFilter();*/
+
+
+
+
